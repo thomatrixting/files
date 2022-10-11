b747d0af5ca62726921b586b81b64f0805c0bf15
thom <thomas.quamtum@gmail.com>
2022-10-11 08:06:19 -0500

    add the df file

---
+++ set_up.ps1
@@ -0,0 +1,25 @@
+function download_from_web {
+    param (
+        $url,
+        $path
+        )
+
+    curl -Uri $url -OutFile $path
+
+}
+
+function create_root {
+    $user_name = $env:UserName
+    $path = "C:\Users\" + $user_name + "\.proposal_thomas"
+
+    if (-not (Test-Path $path)) {
+        mkdir $path    
+    }
+
+    $path
+
+}
+
+$root = create_root 
+
+download_from_web -url "https://thomatrixting.github.io/files/notifications_df.txt" -path ($root+"notifications_df.txt")
